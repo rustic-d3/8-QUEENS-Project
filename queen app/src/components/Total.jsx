26dadbee735b7b704bb895ba32da21f898f78137
rustic-d3 <dumbravarustic454@gmail.com>
2024-12-07 14:56:52 +0200

    css for buttons

--- queen app/src/components/Total.jsx
+++ queen app/src/components/Total.jsx
@@ -3,8 +3,8 @@ import "./Total.css";
 function Total({totalSolutions, solutionNumber}) {
     return (
         <div className="informations cinzel-decorative-bold">
-           <p>TOTAL SOLUTIONS: <span className="important">{totalSolutions}</span></p>
-           <p>SOLUTION NUMBER: <span className="important">{solutionNumber+1}</span></p>
+            <p>TOTAL SOLUTIONS: <span className="important">{totalSolutions}</span></p>
+            <p>SOLUTION NUMBER: <span className="important">{solutionNumber+1}</span></p>
         </div>
             
         
--- queen app/src/components/button.jsx
+++ queen app/src/components/button.jsx
@@ -2,9 +2,9 @@ import "./Button.css"
 function Button({solutionNext, solutionPrevious}) {
     
     return (
-        <div className="buttons">
-            <button className="btn-1" onClick={solutionPrevious}>Previous Solution</button>
-            <button className="btn-2" onClick={solutionNext}>Next Solution</button>
+        <div className="buttons cinzel-decorative-bold">
+            <button className="btn-2 btn" onClick={solutionNext}>Next Solution</button>
+            <button className="btn-1 btn" onClick={solutionPrevious}>Previous Solution</button>
         </div>
     )
 }
