1aa207eb76f5aeb3882df5026e3de41153c73ccb
rustic-d3 <dumbravarustic454@gmail.com>
2024-12-07 04:45:30 +0200

    added buttons color

--- queen app/src/components/button.jsx
+++ queen app/src/components/button.jsx
@@ -3,8 +3,8 @@ function Button({solutionNext, solutionPrevious}) {
     
     return (
         <div className="buttons">
-            <button onClick={solutionPrevious}>Previous Solution</button>
-            <button onClick={solutionNext}>Next Solution</button>
+            <button className="btn-1" onClick={solutionPrevious}>Previous Solution</button>
+            <button className="btn-2" onClick={solutionNext}>Next Solution</button>
         </div>
     )
 }
